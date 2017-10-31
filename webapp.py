0aec8f944b3ec208278e7017d00ed8740f9a70fb
savaxxahx <savannah@katherynsavannah.com>
2017-10-31 09:21:14 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,4 +4,8 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('page1.html')
+
+
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
