e87cdc18fb1c3e9418e38f40d5eff435b4af6839
savaxxahx <savannah@katherynsavannah.com>
2017-10-31 09:44:52 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,6 +2,13 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+
+def get_state_options(counties):
+  options = str( " ")
+  for c in counties
+    if 
+  
+
 @app.route("/")
 def render_main():
     return render_template('page1.html')
