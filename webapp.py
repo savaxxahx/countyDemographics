53460b3a2750e3724bc312791c3a480300973afe
savaxxahx <savannah@katherynsavannah.com>
2017-11-03 09:54:37 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 
-def get_state_options(counties):
+"""def get_state_options(counties):
   states = []
     for c in counties:
         if c["State"] not in states:
@@ -15,14 +15,14 @@ def get_state_options(counties):
     for s in states:
        options+=Markup("<option value=\"" + s + "\">" + s + "</option>")
     return options
-def fun_fact(states):
+def fun_fact(states):"""
    
 
 @app.route("/")
 def render_main():
-    with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    return render_template('page1.html', options = get_state_options(counties))
+    """with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)"""
+    return render_template('page1.html', options = str(" "))
 
 
 if __name__=="__main__":
