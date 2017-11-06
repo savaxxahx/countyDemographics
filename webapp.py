625e8e72ebfc5372c0bccf4ca0e76f8f8fcc1082
savaxxahx <savannah@katherynsavannah.com>
2017-11-06 09:57:27 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,7 +13,7 @@ def get_state_options(counties):
             
   options = Str("")
     for s in states:
-       options+=Markup
+       options+=Markup("<option value=\"" + s + "\">" + s + "</option>")
     return options
 
    
