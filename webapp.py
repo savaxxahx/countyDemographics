3d52fc48e59408ef40bb3ca277497dea3475788e
savaxxahx <savannah@katherynsavannah.com>
2017-11-07 09:49:02 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,12 +8,12 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-   with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
     return render_template('page1.html', states = get_state_options(counties))
 
 
-def get_state_options(counties):
+def get_state_options():
+  with open('county_demographics.json') as demographics_data:
+      counties = json.load(demographics_data)
   states = []
   for c in counties:
       if c["State"] not in states:
