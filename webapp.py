647654722bc170c586962beaf405dbb2d37841d3
savaxxahx <savannah@katherynsavannah.com>
2017-11-09 10:01:22 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -29,14 +29,14 @@ def get_state_options():
   return options
 
 def fun_fact(state):
-  with open('county_demographics.json') as demographics_data:
-      counties = json.load(demographics_data)
-  first = counties[0]["Miscellaneous"]["Percent Female"]
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    first = counties[0]["Miscellaneous"]["Percent Female"]
   """for c in counties:
       if c["counties"] in state:
          if c["Miscellaneous"]["Percent Female"]> first:
             first =c["Miscellaneous"]["Percent Female"]"""
-  return str(first)                   
+    return str(first)                   
    
     
 
