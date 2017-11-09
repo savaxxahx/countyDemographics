4a7ea29eb1d07b8c4140c0d80218e433c8313e39
savaxxahx <savannah@katherynsavannah.com>
2017-11-09 10:48:24 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('page1.html', states= str(""))
+    return render_template('page1.html')
                            
 
 """@app.route("/answer")
